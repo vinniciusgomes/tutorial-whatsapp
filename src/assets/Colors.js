d8d57fd9fa8bc034c4b39909eefb44791d9de327
Felipe Novais <novaisfelipe97@gmail.com>
2019-10-13 15:09:51 -0300

    Criando layout da tela de introdução

---
+++ src/assets/Colors.js
@@ -0,0 +1,12 @@
+const colors = {
+  white: '#ffffff',
+  teal: '#128c7e',
+  lightTeal: '#1ebea5',
+  darkTeal: '#075e54',
+  green: '#25d366',
+  blue: '#34b7f1',
+  chatBg: '#ece5dd',
+  title: '#55636b',
+  text: '#4b4b4b',
+};
+export default colors;
